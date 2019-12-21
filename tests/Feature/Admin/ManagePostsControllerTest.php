350812a9a27c8039d03e319a20a94fa154e9d26f
Ash B <github@webdevetc.com>
2019-12-21 16:04:02 +0000

    Manage Posts - further tests

--- tests/Feature/Admin/ManagePostsControllerTest.php
+++ tests/Feature/Admin/ManagePostsControllerTest.php
@@ -139,6 +139,7 @@ class ManagePostsControllerTest extends TestCase
         $this->assertCount(1, $postCategories);
         $this->assertTrue($postCategories->first()->is($category));
     }
+
     /**
      * Test that new posts can be created, and associated to a category.
      */
@@ -160,4 +161,96 @@ class ManagePostsControllerTest extends TestCase
         $response->assertSessionHasErrors('category');
     }
 
+    /**
+     * Test the edit form.
+     */
+    public function testEdit(): void
+    {
+        $this->beAdminUser();
+
+        $post = factory(Post::class)->create();
+
+        $response = $this->get(route('blogetc.admin.edit_post', $post));
+
+        $response->assertOk()->assertSee($post->title);
+    }
+
+    /**
+     * Test that trying to edit an invalid blog post does not work.
+     */
+    public function testEditInvalidPost(): void
+    {
+        $this->beAdminUser();
+
+        $invalidID = 9999;
+
+        $response = $this->get(route('blogetc.admin.edit_post', $invalidID));
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * Test can delete a post.
+     */
+    public function testDestroy(): void
+    {
+        $this->beAdminUser();
+
+        $post = factory(Post::class)->create();
+
+        $response = $this->delete(route('blogetc.admin.destroy_post', $post));
+
+        $response->assertOk()->assertViewIs('blogetc_admin::posts.deleted_post');
+
+        $this->assertDatabaseMissing('blog_etc_posts', ['id' => $post->id]);
+    }
+
+    /**
+     * Test 404 response when deleting invalid post ID
+     */
+    public function testDestroyInvalidPostID(): void
+    {
+        $this->beAdminUser();
+
+        $invalidPostID = 999;
+        $response = $this->delete(route('blogetc.admin.destroy_post', $invalidPostID));
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * Test can update a post.
+     */
+    public function testUpdate(): void
+    {
+        $this->beAdminUser();
+
+        $post = factory(Post::class)->create();
+
+        $params = $post->toArray();
+
+        // Update title.
+        $params['title'] = $this->faker->sentence;
+
+        $response = $this->patch(route('blogetc.admin.edit_post', $post), $params);
+
+        $response->assertSessionHasNoErrors()->assertRedirect();
+
+        $this->assertDatabaseHas('blog_etc_posts', ['title' => $params['title']]);
+    }
+
+    /**
+     * Test trying to update a post which does not exist gives a 404 response.
+     */
+    public function testUpdateInvalidPostID(): void
+    {
+        $invalidPostID = 10000;
+        $this->beAdminUser();
+
+        $params = factory(Post::class)->make()->toArray();
+
+        $response = $this->patch(route('blogetc.admin.edit_post', $invalidPostID), $params);
+
+        $response->assertNotFound();
+    }
 }
