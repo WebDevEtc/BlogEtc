3fe2307dd507efa4cb2b1a0407ea06f7feada59a
WebDevEtc <fromgithub@webdevetc.com>
2020-05-19 20:16:30 +0100

    Renamed model class names
     - Kept existing class names for backwards compatibility.

--- src/Models/BlogEtcUploadedPhoto.php
+++ src/Models/BlogEtcUploadedPhoto.php
@@ -2,17 +2,10 @@
 
 namespace WebDevEtc\BlogEtc\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class BlogEtcUploadedPhoto extends Model
+/**
+ * legacy class for backwards compatibility.
+ * @deprecated
+ */
+class BlogEtcUploadedPhoto extends UploadedPhoto
 {
-    public $table = 'blog_etc_uploaded_photos';
-    public $casts = [
-        'uploaded_images' => 'array',
-    ];
-    public $fillable = [
-        'image_title',
-        'uploader_id',
-        'source', 'uploaded_images',
-    ];
 }
