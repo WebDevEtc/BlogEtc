e6e4796bdb964e7b67df99f54a76bd3ad7f09eed
WebDevEtc <fromgithub@webdevetc.com>
2020-05-19 21:13:16 +0100

    Update migrations (minor style changes)

--- migrations/2018_09_26_085711_add_short_desc_textrea_to_blog_etc.php
+++ migrations/2018_09_26_085711_add_short_desc_textrea_to_blog_etc.php
@@ -4,28 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// (ignore typo! Don't want to change in case it breaks some people's migrations!)
+
+/**
+ * Class AddShortDescTextreaToBlogEtc.
+ */
 class AddShortDescTextreaToBlogEtc extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
+     * Create the DB table changes to add TEXT short_description column.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::table('blog_etc_posts', function (Blueprint $table) {
+        Schema::table('blog_etc_posts', static function (Blueprint $table) {
             $table->text('short_description')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::table('blog_etc_posts', function (Blueprint $table) {
+        Schema::table('blog_etc_posts', static function (Blueprint $table) {
             $table->dropColumn('short_description');
         });
     }
--- migrations/2018_09_27_122627_create_blog_etc_uploaded_photos_table.php
+++ migrations/2018_09_27_122627_create_blog_etc_uploaded_photos_table.php
@@ -4,38 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class CreateBlogEtcUploadedPhotosTable.
+ */
 class CreateBlogEtcUploadedPhotosTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
+     * Create the DB table for Blog Etc photos.
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('blog_etc_uploaded_photos', function (Blueprint $table) {
+        Schema::create('blog_etc_uploaded_photos', static function (Blueprint $table) {
             $table->increments('id');
             $table->text('uploaded_images')->nullable();
             $table->string('image_title')->nullable();
             $table->string('source')->default('unknown');
             $table->unsignedInteger('uploader_id')->nullable()->index();
+
             $table->timestamps();
         });
-        Schema::table('blog_etc_posts', function (Blueprint $table) {
+        Schema::table('blog_etc_posts', static function (Blueprint $table) {
             $table->string('seo_title')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('blog_etc_uploaded_photos');
 
-        Schema::table('blog_etc_posts', function (Blueprint $table) {
+        Schema::table('blog_etc_posts', static function (Blueprint $table) {
             $table->dropColumn('seo_title');
         });
     }
