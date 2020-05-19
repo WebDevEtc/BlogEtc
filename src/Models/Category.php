5adb0b769e366a1b31f05c3059c31cd61f23761d
WebDevEtc <fromgithub@webdevetc.com>
2020-05-19 20:16:30 +0100

    Renamed model class names
     - Kept existing class names for backwards compatibility.

---
+++ src/Models/Category.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace WebDevEtc\BlogEtc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    public $fillable = [
+        'category_name',
+        'slug',
+        'category_description',
+    ];
+
+    protected $table = 'blog_etc_categories';
+
+    /**
+     * @return BelongsToMany
+     */
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'blog_etc_post_categories');
+    }
+
+    /**
+     * Returns the public facing URL of showing blog posts in this category.
+     *
+     * @return string
+     */
+    public function url(): string
+    {
+        return route('blogetc.view_category', $this->slug);
+    }
+
+    /**
+     * Returns the URL for an admin user to edit this category.
+     *
+     * @return string
+     */
+    public function edit_url(): string
+    {
+        return route('blogetc.admin.categories.edit_category', $this->id);
+    }
+}
---
+++ src/Models/Comment.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace WebDevEtc\BlogEtc\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use WebDevEtc\BlogEtc\Scopes\BlogCommentApprovedAndDefaultOrderScope;
+
+class Comment extends Model
+{
+    public $casts = [
+        'approved' => 'boolean',
+    ];
+
+    public $fillable = [
+        'comment',
+        'author_name',
+    ];
+    protected $table = 'blog_etc_comments';
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        /* If user is logged in and \Auth::user()->canManageBlogEtcPosts() == true, show any/all posts.
+           otherwise (which will be for most users) it should only show published posts that have a posted_at
+           time <= Carbon::now(). This sets it up: */
+        static::addGlobalScope(new BlogCommentApprovedAndDefaultOrderScope());
+    }
+
+    /**
+     * The associated BlogEtcPost.
+     *
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'blog_etc_post_id');
+    }
+
+    /**
+     * Comment author user (if set).
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Return author string (either from the User (via ->user_id), or the submitted author_name value.
+     *
+     * @return string
+     */
+    public function author()
+    {
+        if ($this->user_id) {
+            $field = config('blogetc.comments.user_field_for_author_name', 'name');
+
+            return optional($this->user)->$field;
+        }
+
+        return $this->author_name;
+    }
+}
