eae9a2639f969fbe4b1d277619bf04a97551a5ab
WebDevEtc <fromgithub@webdevetc.com>
2020-05-19 21:13:43 +0100

    Update captcha (minor style changes)

--- src/Captcha/CaptchaAbstract.php
+++ src/Captcha/CaptchaAbstract.php
@@ -4,38 +4,39 @@ namespace WebDevEtc\BlogEtc\Captcha;
 
 use Illuminate\Http\Request;
 use WebDevEtc\BlogEtc\Interfaces\CaptchaInterface;
-use WebDevEtc\BlogEtc\Models\BlogEtcPost;
+use WebDevEtc\BlogEtc\Models\Post;
 
+/**
+ * Class CaptchaAbstract.
+ */
 abstract class CaptchaAbstract implements CaptchaInterface
 {
     /**
      * executed when viewing single post.
      *
-     * @return void
+     * @return mixed|void
      */
-    public function runCaptchaBeforeShowingPosts(Request $request, BlogEtcPost $blogEtcPost)
+    public function runCaptchaBeforeShowingPosts(Request $request, Post $blogEtcPost)
     {
-        // no code here to run! Maybe in your subclass you can make use of this?
         /*
+        No code here to run! Maybe in your subclass you can make use of this?
 
         But you could put something like this -
         $some_question = ...
         $correct_captcha = ...
-        \View::share("correct_captcha",$some_question); // << reference this in the view file.
-        \Session::put("correct_captcha",$correct_captcha);
+        View::share("correct_captcha", $someQuestion); // << reference this in the view file.
+        Session::put("correct_captcha",$correctCaptcha);
 
-
-        then in the validation rules you can check if the submitted value matched the above value. You will have to implement this.
-
+        ...then in the validation rules you can check if the submitted value matched the above value.
         */
     }
 
     /**
      * executed when posting new comment.
      *
-     * @return void
+     * @return void|mixed
      */
-    public function runCaptchaBeforeAddingComment(Request $request, BlogEtcPost $blogEtcPost)
+    public function runCaptchaBeforeAddingComment(Request $request, Post $blogEtcPost)
     {
         // no code here to run! Maybe in your subclass you can make use of this?
     }
