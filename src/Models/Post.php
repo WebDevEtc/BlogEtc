e5dbe153357a06e295135d1334d1a2a775fdd852
WebDevEtc <ashb@webdevetc.com>
2020-05-24 20:33:56 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -85,8 +85,6 @@ class Post extends Model implements SearchResultInterface
         return $this->title;
     }
 
-    /**
-     */
     protected static function boot()
     {
         parent::boot();
