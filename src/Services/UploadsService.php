34e8c5bfdb1f6bd5568f6e76d56f91ab994ceda7
WebDevEtc <ashb@webdevetc.com>
2020-05-30 16:20:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Services/UploadsService.php
+++ src/Services/UploadsService.php
@@ -58,7 +58,7 @@ class UploadsService
 
         $this->legacyProcessUploadedImages($request, $new_blog_post);
 
-        if (! $new_blog_post->posted_at) {
+        if (!$new_blog_post->posted_at) {
             $new_blog_post->posted_at = Carbon::now();
         }
 
@@ -120,7 +120,7 @@ class UploadsService
         }
 
         foreach ((array) config('blogetc.image_sizes') as $size => $image_size_details) {
-            if (! isset($sizes_to_upload[$size]) || ! $sizes_to_upload[$size] || ! $image_size_details['enabled']) {
+            if (!isset($sizes_to_upload[$size]) || !$sizes_to_upload[$size] || !$image_size_details['enabled']) {
                 continue;
             }
 
@@ -149,7 +149,7 @@ class UploadsService
      */
     public function legacyProcessUploadedImages(LegacyGetImageFileInterface $request, Post $new_blog_post)
     {
-        if (! config('blogetc.image_upload_enabled')) {
+        if (!config('blogetc.image_upload_enabled')) {
             return;
         }
 
@@ -246,7 +246,7 @@ class UploadsService
      */
     protected function check_image_destination_path_is_writable($path)
     {
-        if (! is_writable($path)) {
+        if (!is_writable($path)) {
             throw new RuntimeException("Image destination path is not writable ($path)");
         }
     }
@@ -271,13 +271,13 @@ class UploadsService
         $wh = $this->getDimensions($image_size_details);
         $ext = '.'.$photo->getClientOriginalExtension();
 
-        for ($i = 1; $i <= 10; ++$i) {
+        for ($i = 1; $i <= 10; $i++) {
             // add suffix if $i>1
             $suffix = $i > 1 ? '-'.str_random(5) : '';
 
             $attempt = str_slug($base.$suffix.$wh).$ext;
 
-            if (! File::exists($this->image_destination_path().'/'.$attempt)) {
+            if (!File::exists($this->image_destination_path().'/'.$attempt)) {
                 // filename doesn't exist, let's use it!
                 return $attempt;
             }
@@ -293,7 +293,7 @@ class UploadsService
     protected function generate_base_filename(string $suggested_title)
     {
         $base = substr($suggested_title, 0, 100);
-        if (! $base) {
+        if (!$base) {
             // if we have an empty string then we should use a random one:
             $base = 'image-'.str_random(5);
 
@@ -452,13 +452,13 @@ class UploadsService
         $wh = $this->getDimensions($image_size_details);
         $ext = '.'.$photo->getClientOriginalExtension();
 
-        for ($i = 1; $i <= self::$availableFilenameAttempts; ++$i) {
+        for ($i = 1; $i <= self::$availableFilenameAttempts; $i++) {
             // add suffix if $i>1
             $suffix = $i > 1 ? '-'.Str::random(5) : '';
 
             $attempt = Str::slug($base.$suffix.$wh).$ext;
 
-            if (! $this::disk()->exists($this->imageDestinationPath().'/'.$attempt)) {
+            if (!$this::disk()->exists($this->imageDestinationPath().'/'.$attempt)) {
                 return $attempt;
             }
         }
