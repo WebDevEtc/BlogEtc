07e89646e6b92fd1b6fae81ddac62576df1a105e
WebDevEtc <fromgithub@webdevetc.com>
2020-05-30 16:29:27 +0100

    Updated readme & composer versions

---
+++ src/Requests/BaseRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WebDevEtc\BlogEtc\Requests;
+
+/**
+ * @deprecated - use BaseAdminRequest instead.
+ * This exists only for backwards compatibility.
+ * Do not use this for any non admin panel related requests.
+ */
+abstract class BaseRequest extends BaseAdminRequest
+{
+}
