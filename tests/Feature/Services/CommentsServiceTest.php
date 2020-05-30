7773d402317bc3373f19948f1b4728988c968c71
WebDevEtc <fromgithub@webdevetc.com>
2020-05-30 15:30:44 +0100

    Added additional tests

---
+++ tests/Feature/Services/CommentsServiceTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace WebDevEtc\BlogEtc\Tests\Feature;
+
+use Config;
+use Illuminate\Foundation\Testing\WithFaker;
+use WebDevEtc\BlogEtc\Exceptions\CategoryNotFoundException;
+use WebDevEtc\BlogEtc\Exceptions\CommentNotFoundException;
+use WebDevEtc\BlogEtc\Models\Category;
+use WebDevEtc\BlogEtc\Models\Comment;
+use WebDevEtc\BlogEtc\Models\Post;
+use WebDevEtc\BlogEtc\Services\CategoriesService;
+use WebDevEtc\BlogEtc\Services\CommentsService;
+use WebDevEtc\BlogEtc\Tests\TestCase;
+
+class CommentsServiceTest extends TestCase
+{
+    use WithFaker;
+
+    /** @var CommentsService */
+    protected $commentsService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->featureSetUp();
+
+        $this->commentsService = resolve(CommentsService::class);
+        Comment::truncate();
+    }
+
+
+    public function testApprove()
+    {
+        $comment = factory(Comment::class)->create(['approved' => false]);
+
+        $this->commentsService->approve($comment->id);
+
+        $comment->refresh();
+
+        $this->assertTrue($comment->fresh()->approved);
+    }
+
+    /**
+     * Approving an already approved comment should still work.
+     */
+    public function testApproveAlreadyApproved()
+    {
+        $comment = factory(Comment::class)->create(['approved' => true]);
+        $this->commentsService->approve($comment->id);
+        $this->assertTrue($comment->fresh()->approved);
+    }
+
+    public function testApproveNonExistingComment()
+    {
+        $this->expectException(CommentNotFoundException::class);
+        $this->commentsService->approve(0);
+    }
+
+    public function testFind()
+    {
+        $comment = factory(Comment::class)->create();
+
+        $response = $this->commentsService->find($comment->id);
+
+        $this->assertTrue($comment->is($response));
+    }
+
+    public function testFindApproved()
+    {
+        $comment = factory(Comment::class)->create(['approved' => true]);
+        $response = $this->commentsService->find($comment->id, true);
+        $this->assertTrue($comment->is($response));
+    }
+
+    public function testFindNonApproved()
+    {
+        $comment = factory(Comment::class)->create(['approved' => false]);
+        $this->expectException(CommentNotFoundException::class);
+        $this->commentsService->find($comment->id, true);
+    }
+
+    public function testFindNonExisting()
+    {
+        $this->expectException(CommentNotFoundException::class);
+        $this->commentsService->find(0);
+    }
+
+    public function testCreate()
+    {
+        $post = factory(Post::class)->create();
+
+        $commentText = $this->faker->sentence;
+        $this->commentsService->create($post, ['comment' => $commentText], '127.0.0.1', null);
+
+        $this->assertDatabaseHas('blog_etc_comments', ['comment' => $commentText, 'blog_etc_post_id' => $post->id, 'approved' => false]);
+    }
+}
---
+++ tests/Feature/Services/PostsServiceTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace WebDevEtc\BlogEtc\Tests\Feature;
+
+use Carbon\Carbon;
+use Config;
+use Illuminate\Foundation\Testing\WithFaker;
+use WebDevEtc\BlogEtc\Exceptions\CategoryNotFoundException;
+use WebDevEtc\BlogEtc\Exceptions\CommentNotFoundException;
+use WebDevEtc\BlogEtc\Exceptions\PostNotFoundException;
+use WebDevEtc\BlogEtc\Models\Category;
+use WebDevEtc\BlogEtc\Models\Comment;
+use WebDevEtc\BlogEtc\Models\Post;
+use WebDevEtc\BlogEtc\Services\CategoriesService;
+use WebDevEtc\BlogEtc\Services\CommentsService;
+use WebDevEtc\BlogEtc\Services\PostsService;
+use WebDevEtc\BlogEtc\Tests\TestCase;
+
+class PostsServiceTest extends TestCase
+{
+    use WithFaker;
+
+    /** @var PostsService */
+    private $postsService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->featureSetUp();
+
+        $this->postsService = resolve(PostsService::class);
+        Post::truncate();
+    }
+
+
+    public function testIndexPaginated()
+    {
+        factory(Post::class, 25)->create();
+
+        $response = $this->postsService->indexPaginated(10, null);
+
+        $this->assertSame(25, $response->total());
+        $this->assertSame(3, $response->lastPage());
+    }
+
+    public function testIndexPaginatedUnpublished()
+    {
+        factory(Post::class)->create(['is_published' => false]);
+        factory(Post::class)->create(['posted_at' => Carbon::now()->addHour()]);
+
+        $response = $this->postsService->indexPaginated(10, null);
+
+        $this->assertSame(0, $response->total());
+    }
+
+    public function testIndexPaginatedCategoryWithNoPosts()
+    {
+        $category = factory(Category::class)->create();
+
+        factory(Post::class, 25)->create();
+
+        $response = $this->postsService->indexPaginated(10, $category->id);
+
+        $this->assertSame(0, $response->total());
+    }
+
+
+    public function testRssItems()
+    {
+        factory(Post::class, 11)->create();
+
+        $response = $this->postsService->rssItems();
+
+        $this->assertCount(10, $response);
+    }
+
+    public function testRssItemsDoesNotIncludeUnpublished()
+    {
+        factory(Post::class)->create(['is_published' => false]);
+        factory(Post::class)->create(['posted_at' => Carbon::now()->addHour()]);
+
+        $response = $this->postsService->rssItems();
+
+        $this->assertEmpty($response);
+    }
+
+    public function testSearch(): void
+    {
+        [$post1, $post2] = factory(Post::class, 2)->create(['title' => 'an example title']);
+
+        $response = $this->postsService->search($post1->title);
+
+        $this->assertCount(2, $response);
+
+        $this->assertTrue($response[0]->is($post1));
+    }
+
+    public function testSearchDoesNotIncludeUnpublished(): void
+    {
+        factory(Post::class)->create(['title' => 'test-unpublished', 'is_published' => false]);
+        factory(Post::class)->create(['title' => 'test-unpublished', 'posted_at' => Carbon::now()->addDay()]);
+        $published = factory(Post::class)->create(['title' => 'test-unpublished']);
+
+        $response = $this->postsService->search('test-unpublished');
+
+        $this->assertCount(1, $response);
+
+        $this->assertTrue($response[0]->is($published));
+    }
+
+    public function testFindBySlug(): void
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->postsService->findBySlug($post->slug);
+
+        $this->assertTrue($post->is($response));
+    }
+
+    public function testFindBySlugFails(): void
+    {
+        $this->expectException(PostNotFoundException::class);
+        $this->postsService->findBySlug('invalid');
+    }
+
+    public function testFindBySlugFailsWhenEmpty(): void
+    {
+        $this->expectException(PostNotFoundException::class);
+        $this->postsService->findBySlug('');
+    }
+}
+
