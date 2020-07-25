26c2a51a9992680776203e9c4adf20a397152032
WebDevEtc <fromgithub@webdevetc.com>
2020-07-25 18:12:05 +0100

    feat: hide search box if not enabled

--- src/Views/blogetc/sitewide/search_form.blade.php
+++ src/Views/blogetc/sitewide/search_form.blade.php
@@ -1,8 +1,9 @@
-<div style="max-width:500px; margin:50px auto;" class="search-form-outer">
-    <form method="get" action="{{ route('blogetc.search') }}" class="text-center">
-        <h4>Search for something in our blog:</h4>
-        <input type="text" name="s" placeholder="Search..." class="form-control" value="{{ Request::get('s') }}">
-        <input type="submit" value="Search" class="btn btn-primary m-2">
-    </form>
-</div>
-
+@if (config('blogetc.search.search_enabled') )
+    <div style="max-width:500px; margin:50px auto;" class="search-form-outer">
+        <form method="get" action="{{ route('blogetc.search') }}" class="text-center">
+            <h4>Search for something in our blog:</h4>
+            <input type="text" name="s" placeholder="Search..." class="form-control" value="{{ Request::get('s') }}">
+            <input type="submit" value="Search" class="btn btn-primary m-2">
+        </form>
+    </div>
+@endif
