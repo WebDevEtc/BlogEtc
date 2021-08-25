f51c84db20f885c92702df50976ebead598d50e8
BienNguyen <thebienpronguyen2201@gmail.com>
2021-08-25 16:25:06 +0700

    fix: use config user_model in Comment model

--- src/Models/Comment.php
+++ src/Models/Comment.php
@@ -49,7 +49,7 @@ class Comment extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(config('blogetc.user_model', User::class));
     }
 
     /**
